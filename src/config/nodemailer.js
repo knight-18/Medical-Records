ad6da219278444f6d800e1c645f86b665fb48653
Rishi-Raj-Kalita <rishirajkalita13@gmail.com>
2020-10-26 20:20:02 +0530

    made changes to nodemailer.js

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,26 +1,46 @@
-var nodemailer = require("nodemailer");
-import _ from 'lodash';
-exports.sendMessage = function (req, res) {
-  try {
-    const emailToken = jwt.sign(
-      {
-        user: user.id,
-      },
-      process.env.Jwt_Secret,
-      {
-        expiresIn: '30m',
-      },
-    );
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const { JsonWebTokenError } = require("jsonwebtoken");
+const jwt = require("jsonwebtoken")
 
-    const url = `http://localhost:5000/${emailToken}`;//to be modified
+const signupMail = (data) => {
+  const maxAge = 3 * 60 * 60;
+const TOKEN = (data) => {
+  return jwt.sign({ data }, process.env.JWT_SECRET, {
+    expiresIn: maxAge
+  });
+};
+  const host = req.get("host");
+  const PORT= process.env.PORT || 3000;
+  const link = `${req.protocol}://${req.hostname}:${PORT}/user/verify/${data._id}?tkn=${TOKEN}`;
 
-    await transporter.sendMail({
-      from: process.env.NODEMAILER_EMAIL,
-      to: args.email,
-      subject: 'Confirm Email',
-      html: `Please click this link to verify your email: <a href="${url}">Click here</a>`,
-    });
-  } catch (e) {
-    console.log(e);
-  }
+  var transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.NODEMAILER_EMAIL, //email id
+      pass: process.env.NODEMAILER_PASSWORD, //my gmail password
+    },
+  });
+  var mailOptions = {
+    from: process.env.NODEMAILER_EMAIL,
+    to: `${data.email}`,
+    subject: "Please confirm your Email account",
+    html:
+      "Hello,<br> Please here to verify your email.<br><a href=" +
+      link +
+      ">Click here to verify</a>",
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log("Error", error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
+
+module.exports = {
+  signupMail,
 };
