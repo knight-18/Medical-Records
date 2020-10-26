cc1ca79b0fc7fd5e7842b5e0e4669618e4ce1989
Debojyoti1915001 <debojyoti76@gmail.com>
2020-10-26 18:29:21 +0530

    modified nodemailer.js->added a function signupMail to send email to the user for email verification

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,20 +1,7 @@
-const express = require("express");
-const router = express.Router();
 const User = require("../models/User");
-const authorization = require("../middleware/auth");
-const uuid = require("uuid");
-const fs = require("fs");
-const path = require("path");
-const async = require("async");
-const bodyParser = require("body-parser");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const cookieParser = require('cookie-parser');
-const { check, validationResult } = require('express-validator');
-const flash = require('connect-flash');
-const session = require("express-session");
-const nodemailer = require('nodemailer');
-const cryptoRandomString = require('crypto-random-string');
+const jwt = require('jsonwebtoken');
+//const {signupMail}=require('../config/nodemailer')
+
 require('dotenv').config();
 
 const maxAge = 30 * 24 * 60 * 60;
@@ -48,80 +35,6 @@ const handleErrors = (err) => {
  
 }
 
-//nodemailer methods
-
-var transporter = nodemailer.createTransport({
-  host:'smtp.gmail.com',
-  port:465,
-  secure:true,
-  auth: {
-    user: process.env.NODEMAILER_EMAIL,           //email id
-    pass: process.env.NODEMAILER_PASSWORD       //my gmail password
-  }
-});
-
-
-var rand,mailOptions,host,link;
-/*------------------SMTP Over-----------------------------*/
-
-/*------------------Routing Started ------------------------*/
-
-
-module.exports.verify = (req, res) =>{
-// console.log(req.protocol+":/"+req.get('host'));
-
-  if((req.protocol+"://"+req.get('host'))==("http://"+host))
-  {
-      console.log("Domain is matched. Information is from Authentic email");
-
-      User.findById(req.params.id,function(err,user){
-          if(err)
-              console.log(err);
-          else
-          {
-              date2 = new Date();
-              date1 = user.created_at;
-              var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-              var diffhrs = Math.ceil(timeDiff / (1000 * 60));
-              console.log(diffhrs);
-
-              if(diffhrs <= 3)
-              {
-                  User.findByIdAndUpdate(user._id,{active:true},function(err,user){
-                      if(err)
-                          console.log(err);
-                      else
-                      {
-                          console.log("email is verified");
-                          // res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
-                          res.render("verify");
-                      }
-                        
-                  });
-
-              }
-              else
-              {
-                  User.findByIdAndUpdate(user._id,{created_at: new Date()},function(err,user){
-                      if(err)
-                          console.log(err);   
-                  });
-                  console.log("Link has expired try logging in to get a new link");
-                  // res.end("<h1>Link has expired try logging in to get a new link</h1>");
-                  res.render("notverified");
-              }
-          }
-      });
-  }
-  else
-  {
-      res.end("<h1>Request is from unknown source");
-  }
-};
-
-//==============================
-
-
 // controller actions
 module.exports.signup_get = (req, res) => {
   res.render('signup');
@@ -134,118 +47,80 @@ module.exports.login_get = (req, res) => {
 
 module.exports.signup_post = async (req, res) => {
  
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-      return res.status(422).jsonp(errors.array());
-  }
-  else {
-      bcrypt.hash(req.body.password, 10).then((hash) => {
-          const user = new User({
-              name: req.body.name,
-              email: req.body.email,
-              password: hash
-          });
-          user.save().then((response) => {
-
-              //nodemailer
-              rand=cryptoRandomString({length: 100, type: 'url-safe'});
-              host=req.get('host');
-              link="http://"+req.get('host')+"/dsc/user/verify/"+user._id+"?tkn="+rand;
-              mailOptions={ 
-                  from: process.env.NODEMAILER_EMAIL,
-                  to: user.email,
-                  subject : "Please confirm your Email account",
-                  html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
-              }
-              // console.log(mailOptions);
-              transporter.sendMail(mailOptions, function(error, response){
-               if(error){
-                      console.log(error);
-                  res.end("error");
-               }else{
-                      console.log("Message sent: " + response.message);
-                   }
-              });
-          //nodemailer ends
-              res.locals.flashMessages = req.flash("success", user.name + " Email has been sent to you for verification");
-              res.redirect("/dsc/");
-          }).catch(error => {
-              // res.status(500).json({
-              //     error: error
-              // });
-              // console.log(error);
-              res.locals.flashMessages = req.flash("error", "Email already in use try logging in");
-              res.redirect("/dsc/");
-          });
-      });
-  }
+  const { name, email, password, phoneNumber } = req.body; 
 
  
+
+  try {
+    const user = new User({email, name, password, phoneNumber}); 
+    let saveUser = await user.save(); 
+    const token = createToken(saveUser._id.toString());
+    //signupMail(saveUser)
+    res.cookie('jwt', token, { httpOnly: false, maxAge : maxAge*1000 });
+    console.log(saveUser); 
+    req.flash("success_msg", "Registration Successful now verify your email");
+    res.redirect("/"); 
+  }
+  catch(err) {
+    const errors = handleErrors(err); 
+    console.log(errors);
+    //res.json(errors);
+    req.flash("error_msg", "Could not signup");
+    res.status(400).redirect("/signup");
+  } 
+  
+ 
 }
+module.exports.emailVerify_get=async(req,res)=>{
+  if(req.protocol + "://" + req.get("host") == "http://" + req.get("host")){
+  const token=req.params.id
+  const decoded=jwt.verify(token,process.env.JWT_SECRET)
+  const user=await User.findOne({phoneNumber:decoded.phoneNumber})
+  if(!user)
+  {
+      console.log("user not found")
+      res.redirect("/")
+  }
+  else{
+    const activeUser=await User.findByIdAndUpdate(user._id,{active:true})
+    if(!activeUser)
+    {
+      console.log("Error occured while verifying")
+      req.flash("error_msg","error occured while verifying")
+      res.redirect("/")
+    }
+    else{
+      req.flash("succes_msg","User has been verified")
+      console.log("the user has been verified")
+    }
+  }
+  }
+  else
+  {
+    console.log("Unverified source")
+    res.redirect("/")
+  }
+
+}
+
 
 module.exports.login_post = async (req, res) => {
-  let getUser;
-  User.findOne({
-      email: req.body.email
-  }).then(user => {
-      if (!user) {
-          req.flash("error","User not found try creating a new account");
-          res.redirect("/dsc/");
-      }
-      getUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-  }).then(response => {
-      if (!response) {
-          req.flash("error","You have entered wrong password");
-          res.redirect("/dsc/");
-      }
-      if(getUser.active)
-      {
-          var token = jwt.sign({
-              name: getUser.name,
-              email: getUser.email,
-              userId: getUser._id
-          },process.env.JWT_SECRET, {
-              expiresIn: "1d"
-          });
-          res.cookie( 'authorization', token,{ maxAge: 24*60*60*1000, httpOnly: false });
-      }
-      if(getUser.active)
-      {
-          req.flash("success",getUser.name + " you are logged in");
-          res.redirect("/");
-      }
-      else
-      {
-          rand=cryptoRandomString({length: 100, type: 'url-safe'});
-              host=req.get('host');
-              link="http://"+req.get('host')+"/dsc/user/verify/"+getUser._id+"?tkn="+rand;
-              mailOptions={ 
-                  from: process.env.NODEMAILER_EMAIL,
-                  to: getUser.email,
-                  subject : "Please confirm your Email account",
-                  html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
-              }
-              // console.log(mailOptions);
-              transporter.sendMail(mailOptions, function(error, response){
-               if(error){
-                      console.log(error);
-                  res.end("error");
-               }else{
-                      console.log("Message sent: " + response.message);
-                   }
-              });
-          req.flash("error",getUser.name + " your email is not verified we have sent you an email");
-          res.redirect("/dsc/");
-      }
-      
-  }).catch(err => {
-      req.flash("error",err);
-      res.redirect("/dsc/");
-  });
-}
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id); 
+    res.cookie('jwt', token, { httpOnly: true, maxAge : maxAge * 1000});
+    console.log(user); 
+    req.flash("success_msg", "Successfully logged in");
+    res.status(200).redirect("/profile");
+  } 
+  catch (err) {
+    req.flash("error_msg", "Invalid Credentials"); 
 
+    res.redirect("/login");
+  }
+
+}
 
 
 
@@ -255,8 +130,8 @@ module.exports.profile_get = async (req, res) => {
 }
 
 module.exports.logout_get = async (req, res) => {
-  res.cookie('jwt', '', { maxAge: 1 });
+  // res.cookie('jwt', '', { maxAge: 1 });
+  res.clearCookie("jwt");
   req.flash("success_msg", "Successfully logged out");
   res.redirect('/login');
 }
-
