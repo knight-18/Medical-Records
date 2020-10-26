376282148e91c42fe314c5a6436aa7a4b7424982
Rishi-Raj-Kalita <rishirajkalita13@gmail.com>
2020-10-26 20:26:48 +0530

    resolved the errors in src/app.js

--- src/app.js
+++ src/app.js
@@ -1,68 +1,39 @@
-//Importing packages
-const express = require('express')
-const path = require('path')
-const mongoose = require('mongoose')
-const connect_flash = require('connect-flash')
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
-const expressLayouts = require('express-ejs-layouts')
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
 //Configuring App
-const app = express()
-app.use(express.json())
-app.use(express.static('public'))
-app.use(cookieParser())
+const app = express();
+app.use(express.json());
+
 // using dotenv module for environment
-require('dotenv').config({ path: 'env' })
+require("dotenv").config();
 
 //Configuring Port
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 //Mongoose connection
-mongoose.connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-    })
-    .then(() => console.log('Connected to mongo server'))
-    .catch((err) => console.error(err))
+mongoose.connect(process.env.MONGODB_URL,{
+	useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false,
+    useCreateIndex:true
+}).then(()=>console.log('Connected to mongo server'))
+	.catch(err => console.error(err));
 
 //Setting EJS view engine
-app.set('view engine', 'ejs')
-app.use(expressLayouts)
+app.set('view engine','ejs');
+
 //body parser
-app.use(express.urlencoded({ extended: true }))
-app.use(
-    session({
-        secret: process.env.JWT_SECRET,
-        resave: true,
-        saveUninitialized: true,
-    })
-)
-
-app.use(connect_flash())
-
-// global var
-app.use((req, res, next) => {
-    res.locals.success_msg = req.flash('success_msg')
-    res.locals.error_msg = req.flash('error_msg')
-
-    next()
-})
+app.use(express.urlencoded({extended:true}));
 
 //Setup for rendering static pages
-const publicDirectory = path.join(__dirname, '../public')
-app.use(express.static(publicDirectory))
+const publicDirectory = path.join(__dirname,'../public');
+app.use(express.static(publicDirectory));
 
 //Routes
-const indexRoutes = require('./routes/index')
-const routes = require('./routes/user')
-app.use('/api', indexRoutes)
-
-//Routes
-app.use(routes)
 
 //Start the server
-app.listen(PORT, () => {
-    console.log('Server listening on port', PORT)
+app.listen(PORT,()=>{
+	console.log('Server listening on port',PORT);
 })
