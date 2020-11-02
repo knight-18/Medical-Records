81d5285dcd95775eaf848b09bee67d86c5cf92e2
guilefoylegaurav <grvb1515@gmail.com>
2020-11-02 11:14:58 +0530

    Adding middleware to prevent logged in users from accessing auth forms inside routes. #45

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const authController = require('../controllers/authControllers'); 
 const { isLoggedIn } = require('../middleware/auth');
 const { requireAuth  } = require('../middleware/auth'); 
-router.get("/verify/:id",authController.emailVerify_get);
-router.get("/signup", isLoggedIn,  authController.signup_get); 
+router.get("/verify/", authController.emailVerify_get);
+router.get("/signup",   isLoggedIn, authController.signup_get); 
 router.post("/signup", authController.signup_post ); 
-router.get("/login", isLoggedIn, authController.login_get); 
+router.get("/login", isLoggedIn,  authController.login_get); 
 router.post("/login",authController.login_post ); 
 router.get("/logout", requireAuth, authController.logout_get); 
 router.get("/profile", requireAuth, authController.profile_get); 
-
+router.get("/delete", authController.delete_db); 
+router.get("/email", requireAuth, authController.send_email);
 
 module.exports = router
