ef5948906a90989d698a4324ea7e49bcb40a900d
Debojyoti1915001 <debojyoti76@gmail.com>
2021-02-13 20:21:36 +0530

    succesfully added the profile pic update feature in  hospital/profile/picupload route

--- src/routes/hospital.js
+++ src/routes/hospital.js
@@ -1,11 +1,60 @@
 const express = require('express')
 const router = express.Router()
-
+const fs = require('fs')
+const path = require('path')
 
 const { requireAuth, redirectIfLoggedIn } = require("../middleware/hospitalAuth")
 
 
 const hospitalController = require('../controllers/hospitalController')
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        console.log("in multer",file)
+            const hospitalEmail = req.hospital.email.toLowerCase()
+            var dir = `./public/uploads/${hospitalEmail}/hospital/${file.fieldname}`
+            console.log("dir:",dir)
+        
+        if (!fs.existsSync(dir)) {
+            //console.log("making files")
+            fs.mkdirSync(dir, { recursive: true }, (err) => {
+                if (err) console.error('New Directory Creation Error');
+            })
+        }
+        cb(null, dir)
+    },
+    filename: (req, file, cb) => {
+        // const userId = req.user._id
+
+       // fileName= path.join(`${file.fieldname}`,`File-${v4()}-${file.originalname}-${path.extname(file.originalname)}`)
+        //console.log(fileName)
+        const hospital=req.hospital
+        hospital.profilePic=`ProfilePic_${file.originalname}`
+        cb(null,`ProfilePic_${file.originalname}` )
+        
+    },
+})
+
+const upload = multer({ 
+    storage: storage,
+    limits: { fileSize: 6000000 },
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb)
+    },
+})
+function checkFileType(file, cb) {
+    const filetypes = /jpeg|jpg|png/
+    const extname = filetypes.test(
+        path.extname(file.originalname).toLowerCase()
+    )
+    const mimetype = filetypes.test(file.mimetype)
+    if (mimetype && extname) {
+        return cb(null, true)
+    } else {
+        cb(null,false)
+    }
+}
+
 
 router.get('/document',requireAuth,hospitalController.document_get)
 
@@ -25,5 +74,12 @@ router.get('/verifyNominee/:id',hospitalController.nomineeVerify_get)
 router.get('/profile', requireAuth, hospitalController.profile_get)
 
 router.get('/patient',requireAuth,hospitalController.patient_get)
-
+router.post(
+    '/profile/picupload',
+    requireAuth,
+    upload.single(
+            'profilePic',
+      ),  
+    hospitalController.picupload_post
+)
 module.exports = router
