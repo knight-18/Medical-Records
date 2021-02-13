a0709272c7e61f6700bd81c42f4cf42254b8cbb5
Aditya Agarwal <aditya.agarwal.20012712@gmail.com>
2021-02-13 21:03:32 +0530

    user sidenav of hospitals state remains same as before, i.e. if it was open before searching then it will remain open, otherwise close

--- public/js/sideNav.js
+++ public/js/sideNav.js
@@ -9,11 +9,17 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
+  if(!sessionStorage.getItem('hospitalTab')){
+    sessionStorage.hospitalTab = '0';
+  }
+  document.getElementById("mySidenavTab").style.width = sessionStorage.hospitalTab;
   function openNavtab() {
     document.getElementById("mySidenavTab").style.width = "250px";
+    sessionStorage.hospitalTab = "250px";
   }
   
   /* Set the width of the side navigation to 0 */
   function closeNavtab() {
     document.getElementById("mySidenavTab").style.width = "0";
+    sessionStorage.hospitalTab = "0";
   }
